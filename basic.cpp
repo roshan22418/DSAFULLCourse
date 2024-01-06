5bf331cb1dffac18abad792c2c79db2e5a689431
roshan22418 <roshan22418@iiitd.ac.in>
2024-01-06 22:49:45 +0530

    add some extra recurrsion function

--- basic.cpp
+++ basic.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<limits.h>
+#include <vector>
 using namespace std;
 
 int fibonacciSeries(int n){
@@ -19,6 +21,22 @@ int sum(int n){
         return 1;
     }
     return n+sum(n-1);
+}
+
+void minElement(int *arr, int n,int index,int &min){
+    if(n==index){
+        return; 
+    }
+    if (arr[index]<min)
+    {
+        min= arr[index];
+    }
+    index++;
+    
+    minElement(arr,n,index,min);
+    
+
+
 }
 //check element present or not
 int checkElement(int *arr,int n,int element,int index){
@@ -50,12 +68,45 @@ void printArray(int arr[],int n,int index){
     printArray(arr,n,index+1);
 }
 
+void insertElement(int *arr,int n,int index, vector<int> &ans){
+    cout<<"enter in first stage"<<endl;
+
+    if(n==index){
+        cout<<"enter in final return"<<endl;
+        return;
+    }
+    if (arr[index]%2==0)
+    {
+       
+        ans.push_back(arr[index]);
+        cout<<"enter in if condition"<<endl;
+    }
+    index++;
+    insertElement(arr,n,index,ans);
+    
+
+            
+}
+
 
 int main(){
     // cout<<fibonacciSeries(4)<<endl;
     // cout<<sum(10)<<endl;
-    int array[] = {1,2,3,4,5,6,7};
-    printArray(array,7,0);
-    cout<<checkElement(array,7,9,0)<<endl;
+    int min = INT_MAX;
+    int array[] = {9,2,3,4,-5,6,7};
+    int n = sizeof(array)/sizeof(array[0]);
+    // printArray(array,7,0);
+    // cout<<checkElement(array,7,9,0)<<endl;
+    // minElement(array,n,0,min);
+    // cout<<min<<endl;
+    vector<int> finalAns ;
+    insertElement(array,n,0,finalAns);
+    int length = finalAns.size();
+    for (int i = 0; i < length; i++)
+    {
+        cout<<finalAns[i];
+    }
+    
+
     return 0;
 }
