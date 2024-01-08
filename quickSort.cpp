27a7f4b18e5aa51c1e7099e9a3aa8022c1d3aadd
roshan22418 <roshan22418@iiitd.ac.in>
2024-01-08 22:26:48 +0530

    add mergesort and quick sort

---
+++ quickSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void quicksort(int *arr, int start,int end){
+    if(start>=end){
+        return;
+    }
+    int pivot = end;
+    int i = start-1;
+    int j = start;
+    while (j<pivot)
+    {
+        if (arr[j]<arr[pivot])
+        {
+           i++;
+           swap(arr[i],arr[j]);
+        }
+        j++;
+        
+    }
+    i++;
+    swap(arr[i],arr[pivot]);
+    quicksort(arr,start,i-1);
+    quicksort(arr,i+1,end);
+     
+}
+
+int main(){
+    int arr[] = {4,2,3,6,1,4,7,34,3,-2,-2332,-4343};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    quicksort(arr,0,size-1);
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+    
+
+}
