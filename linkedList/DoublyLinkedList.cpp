2cda51471fe23ee0eeed9ff39e0b197af6ade145
roshan22418 <roshan22418@iiitd.ac.in>
2025-05-23 11:31:30 +0530

    add a doubly linkedlist deletion

--- linkedList/DoublyLinkedList.cpp
+++ linkedList/DoublyLinkedList.cpp
@@ -1,57 +1,55 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 
 class Node
 {
 public:
     int data;
-    Node* prev;
-    Node* next;
-    Node(){
+    Node *prev;
+    Node *next;
+    Node()
+    {
         this->prev = NULL;
         this->next = NULL;
     }
-    Node(int data){
+    Node(int data)
+    {
         this->data = data;
         this->prev = NULL;
         this->next = NULL;
     }
-    ~Node(){
-        cout<<"call the deconstructor"<<endl;
+    ~Node()
+    {
+        cout << "call the deconstructor" << endl;
     }
 };
 
-
-
-void PrintLinkedList(Node* head){
-    Node* temp = head;
-    while (temp!=NULL)
+void PrintLinkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
     {
-        cout<<temp->data<<"->";
+        cout << temp->data << "->";
         temp = temp->next;
     }
-    
 }
 
-int returnLength(Node* head){
-    Node* temp = head;
+int returnLength(Node *head)
+{
+    Node *temp = head;
     int length = 0;
-    while (temp!=NULL)
+    while (temp != NULL)
     {
         // cout<<temp->data<<"->";
-        length+=1;
+        length += 1;
         temp = temp->next;
     }
     return length;
-    
 }
-
-
 
 //  else if(Position==length){
 //         Node* temp = head;
-//         while (temp->next->next ==NULL) 
+//         while (temp->next->next !=NULL)
 //         {
 //             temp= temp->next;
 //         }
@@ -74,37 +72,96 @@ int returnLength(Node* head){
 //         prev->next = curr->next;
 //         curr->next->prev = prev;
 //         delete curr;
-        
+
 //     }
 
-void insertionLinkedList(Node* &head,int data,int Position){
-    Node* newNode = new Node(data);
+
+
+
+void DeleteLinkedList(Node* &head,int position){
     int length = returnLength(head);
+    cout<<"print the position "<<position<<" "<<head->data<<endl;
     if (head==NULL)
+    {
+        // cout<<"delete the temp"<<endl; 
+        cout<<"cannot delete due to empty linkedList"<<endl;
+        return;
+    }
+    else if (position==1)
+    {
+        // cout<<"delete the temp"<<endl;
+        Node* temp = head;
+        head = head->next;
+        temp->next = NULL;
+        // cout<<"delete the temp 2"<<endl;
+        // head = head->next;
+        // cout<<"delete the temp 3"<<endl;
+        head->prev = NULL;
+        // cout<<"delete the temp 4"<<endl;  
+        delete temp;      
+    }
+    else if(position==length){
+        Node* temp = head;
+        while (temp->next->next!=NULL)
+        {
+            temp = temp->next;            
+        }
+        Node* newTemp = temp->next;
+        temp->next = NULL;
+        newTemp->prev = NULL;
+        delete newTemp;       
+
+    }
+    else{
+        Node* prev1 = NULL;
+        Node* curr = head;
+        while (position!=1)
+        {
+            prev1 = curr;
+            curr = curr->next;
+            position--;
+        }
+        prev1->next = curr->next;
+        curr->next->prev = prev1;
+        curr->next = NULL;
+        curr->prev = NULL;
+        delete curr;       
+        
+    }
+    
+    
+
+}
+
+void insertionLinkedList(Node *&head, int data, int Position)
+{
+    Node *newNode = new Node(data);
+    int length = returnLength(head);
+    if (head == NULL)
     {
         head = newNode;
     }
-    else if (Position==1)
+    else if (Position == 1)
     {
         newNode->next = head;
         head->prev = newNode;
-        head = newNode;        
+        head = newNode;
     }
-    else if(Position==length+1){
-        Node* temp = head;
-        while (temp->next!=NULL)
+    else if (Position == length + 1)
+    {
+        Node *temp = head;
+        while (temp->next != NULL)
         {
             temp = temp->next;
         }
         temp->next = newNode;
-        newNode->prev = temp;       
-        
+        newNode->prev = temp;
     }
     else
     {
-        Node* prev = NULL;
-        Node* curr = head;
-        while (Position!=1)
+        Node *prev = NULL;
+        Node *curr = head;
+        while (Position != 1)
         {
             prev = curr;
             curr = curr->next;
@@ -114,30 +171,25 @@ void insertionLinkedList(Node* &head,int data,int Position){
         newNode->prev = prev;
         newNode->next = curr;
         curr->prev = newNode;
-        
-    }   
-    
-
-    
-    
+    }
 }
 
-int main(){
+int main()
+{
     // Node* first = new Node(30);
     // Node* second = new Node(40);
     // first->next = second;
     // second->prev = first;
-    Node* head = NULL;
+    Node *head = NULL;
     // insertionLinkedList(head,3,1);
-    insertionLinkedList(head,2,1);
-    insertionLinkedList(head,4,1);
-    insertionLinkedList(head,4,2);
-    insertionLinkedList(head,4,4);
-    insertionLinkedList(head,7,3);
+    insertionLinkedList(head, 2, 1);
+    insertionLinkedList(head, 4, 1);
+    insertionLinkedList(head, 4, 2);
+    insertionLinkedList(head, 4, 4);
+    insertionLinkedList(head, 7, 3);
+    // DeleteLinkedList(head,2);
+    // DeleteLinkedList(head,5);
+    DeleteLinkedList(head,1);
     PrintLinkedList(head);
     return 0;
-
-
 }
-
-
